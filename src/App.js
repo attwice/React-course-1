f1885ed029ee2a1c6c9e41c8a0b3f6b197964c30
Billibear <93044176+attwice@users.noreply.github.com>
2022-03-23 17:57:43 +0900

    React basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+function greeting(name) {
+  const date = new Date()
+  const hours = date.getHours()
+
+  let timeOfDay
+  if (hours >= 4 && hours < 12) {
+    timeOfDay = "morning"
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon"
+  } else if (hours >= 17 && hours < 20) {
+    timeOfDay = "evening"
+  } else {
+    timeOfDay = "night"
+  }
+
+  return `Good ${timeOfDay}, ${name}!`
+}
+
+
+
 
 function App() {
+
+  const [count, setCount] = React.useState(0)
+
+  function add() {
+    setCount(prevCount => prevCount + 1)
+  }
+  // Challenge: update `substract` to use a callback function
+
+  function subtract() {
+    setCount(prevCount => prevCount - 1)
+  }
+
+  const [things, setThings] = React.useState(["Thing 1", "Thing 2"])
+
+  function addItem() {
+    const newThingText = `Thing ${things.length + 1}`
+    setThings(prevState => [...prevState, newThingText])
+  }
+
+  const thingsElements = things.map(thing => <p key={thing}>{thing}</p>)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{greeting("Bob")}</p>
+      <button onClick={addItem}>Add Item</button>
+      {thingsElements}
+
+      <button className="counter--minus" onClick={subtract}>–</button>
+      <div className="counter--count">
+        <h1>{count}</h1>
+      </div>
+      <button className="counter--plus" onClick={add}>+</button>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Board.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+}
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square" onClick={function() {console.log('click');}}>
+        {this.props.value}
+      </button>
+    );
+  }
+}
